447b5ac5a8710c4ccfc8856b5157412e93e24453
Alieu Jagne <ajagne01@gmail.com>
2023-12-06 17:25:49 +0100

    first part to check for the app

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,6 +14,36 @@ export default function Index() {
       <p className="mt-2 text-lg text-gray-400">
         Planing and Workouts, Updated weekly
       </p>
+
+      <div className="mt-4">
+        <p className="font-bold">
+          Week of December 15<sup>th</sup>
+        </p>
+        <div className="mt-3 space-y-4">
+          <div>
+            <p>Workout</p>
+            <ul className="ml-8 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+              <li>Third item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Program</p>
+            <ul className="ml-8 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+          <div>
+            <p>Interesting things</p>
+            <ul className="ml-8 list-disc">
+              <li>First item</li>
+              <li>Second item</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
