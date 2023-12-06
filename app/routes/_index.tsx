f06e8d639ab2224f0aed7d58f97e9eeb6d52d373
Alieu Jagne <ajagne01@gmail.com>
2023-12-06 20:03:55 +0100

    just tested the form input from tailwind everything good✅

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,61 @@ export default function Index() {
       <p className="mt-2 text-lg text-gray-400">
         Planing and Workouts, Updated weekly
       </p>
+      <div className="my-8 border p-3 ">
+        <form>
+          <p className="italic">Add a new workout or planing</p>
+          <div className="mt-4">
+            <input type="date" name="date" className="text-gray-700" />
+          </div>
 
-      <div className="mt-4">
+          <div className="mt-2 space-x-6">
+            <label>
+              <input
+                className="mr-1"
+                type="radio"
+                name="category"
+                value="workout"
+              />
+              Workout
+            </label>
+            <label>
+              <input
+                className="mr-1"
+                type="radio"
+                name="category"
+                value="planing"
+              />
+              Planing
+            </label>
+            <label>
+              <input
+                className="mr-1"
+                type="radio"
+                name="category"
+                value="interesting-things"
+              />
+              Interesting things
+            </label>
+          </div>
+          <div className="mt-2">
+            <textarea
+              name=""
+              className="w-full text-gray-700"
+              placeholder="Write your workout..."
+            />
+          </div>
+          <div className="mt-1 text-right">
+            <button
+              className="bg-blue-500 px-4 py-1 font-medium text-white"
+              type="submit"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="mt-6">
         <p className="font-bold">
           Week of December 15<sup>th</sup>
         </p>
