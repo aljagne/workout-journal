2f736963a53bcbcfae863e8633cbb3251416f0da
Alieu Jagne <ajagne01@gmail.com>
2023-12-22 20:19:57 +0100

    Prisma and the fetch Fn is good now ✅

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
-import { redirect, type MetaFunction, type ActionFunctionArgs } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import {
+  redirect,
+  type MetaFunction,
+  type ActionFunctionArgs,
+} from "@remix-run/node";
+import { Form, useFetcher } from "@remix-run/react";
 import { PrismaClient } from "@prisma/client";
-
-
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,22 +15,34 @@ export const meta: MetaFunction = () => {
 
 export async function action({ request }: ActionFunctionArgs) {
   let db = new PrismaClient();
-  // console.log(request);
+
+  await new Promise(resolve => setTimeout(resolve, 1000))
+
   let formData = await request.formData();
-  let data = Object.fromEntries(formData.entries());
-  console.log(data);
+  let { date, type, text } = Object.fromEntries(formData);
+  if (
+    typeof date !== "string" ||
+    typeof type !== "string" ||
+    typeof text !== "string"
+  ) {
+    throw new Error("Bad request");
+  }
 
   await db.entry.create({
     data: {
-      date: new Date("2023-12-07"),
-      type: "workout",
-      text: "some text",
+      date: new Date(date),
+      type: type,
+      text: text,
     },
   });
   return redirect("/");
 }
 
 export default function Index() {
+  let fetcher = useFetcher();
+
+  console.log(fetcher.state);
+
   return (
     <div className="p-10">
       <h1 className="text-5xl">Workout Journal</h1>
@@ -36,46 +50,56 @@ export default function Index() {
         Planing and Workouts, Updated weekly
       </p>
       <div className="my-8 border p-3 ">
-        <Form method="post">
-          <p className="italic">Add a new workout or planing</p>
-          <div className="mt-4">
-            <input type="date" name="date" className="text-gray-700" />
-          </div>
+        <p className="italic">Add a new workout or planing</p>
 
-          <div className="mt-2 space-x-6">
-            <label>
+        <fetcher.Form method="post" className="mt-2">
+          <div>
+            <div className="mt-4">
               <input
-                className="mr-1"
-                type="radio"
-                name="category"
-                value="workout"
+                type="date"
+                name="date"
+                required
+                className="text-gray-700"
               />
-              Workout
-            </label>
-            <label>
-              <input
-                className="mr-1"
-                type="radio"
-                name="category"
-                value="planing"
-              />
-              Planing
-            </label>
-            <label>
-              <input
-                className="mr-1"
-                type="radio"
-                name="category"
-                value="interesting-things"
-              />
-              Interesting things
-            </label>
+            </div>
+
+            <div className="mt-2 space-x-6">
+              <label className="inline-block">
+                <input
+                  required
+                  type="radio"
+                  className="mr-1"
+                  name="type"
+                  value="workout"
+                />
+                Workout
+              </label>
+              <label className="inline-block">
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="type"
+                  value="planing"
+                />
+                Planing
+              </label>
+              <label className="inline-block">
+                <input
+                  className="mr-1"
+                  type="radio"
+                  name="type"
+                  value="interesting-things"
+                />
+                Interesting things
+              </label>
+            </div>
           </div>
           <div className="mt-2">
             <textarea
               name="text"
               className="w-full text-gray-700"
               placeholder="Write your workout..."
+              required
             />
           </div>
           <div className="mt-1 text-right">
@@ -83,10 +107,10 @@ export default function Index() {
               className="bg-blue-500 px-4 py-1 font-medium text-white"
               type="submit"
             >
-              Save
+              {fetcher.state === "submitting" ? "Saving..." : "Save"}
             </button>
           </div>
-        </Form>
+        </fetcher.Form>
       </div>
 
       <div className="mt-6">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        gray: colors.neutal,
+        gray: colors.neutral,
       },
     },
   },
